7714c09c592dbf0742390af3a1d8b3e81c62a9ff
anani <abood.anani57@gmail.com>
2022-09-21 19:01:45 +0300

    added game alerts ad reset

--- great_num_game/numgame/urls.py
+++ great_num_game/numgame/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.guess_game),
-    path('guess/', views.play)
+    path('guess/', views.play),
+    path('reset', views.reset)
 ]
--- great_num_game/numgame/views.py
+++ great_num_game/numgame/views.py
@@ -3,10 +3,6 @@ import random
 
 
 def guess_game(request):
-    random_num = random.randint(1, 101)
-    if 'num' not in request.session:
-        request.session['num'] = random_num
-
     return render(request, 'numgame/index.html')
 
 
@@ -21,14 +17,26 @@ def context(text, color):
 
 
 def play(request):
+    random_guess = random.randint(1, 101)
+    if 'num' not in request.session and 'tries' not in request.session:
+        request.session['num'] = random_guess
+        request.session['tries'] = 0
     random_num = request.session['num']
     guess = request.POST['guess']
     guess = int(guess)
-
     if guess == random_num:
         request.session.clear()
         return render(request, 'numgame/index.html', context('wow you guessed it right', 'green'))
-    if guess < random_num:
+    elif guess < random_num and request.session['tries'] < 5:
+        request.session['tries'] += 1
         return render(request, 'numgame/index.html', context('too Low', 'blue'))
-    if guess > random_num:
+    elif guess > random_num and request.session['tries'] < 5:
+        request.session['tries'] += 1
         return render(request, 'numgame/index.html', context('too High', 'red'))
+    if guess != random_num and request.session['tries'] >= 5:
+        return render(request, 'numgame/index.html', context('Game over number of tries exceeded please restart the game', 'purple'))
+
+
+def reset(request):
+    request.session.clear()
+    return redirect('/')
