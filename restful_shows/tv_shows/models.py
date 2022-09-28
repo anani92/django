cadb60230e5d8ba88bd25e72625a4845d98d272a
anani <abood.anani57@gmail.com>
2022-09-28 15:58:57 +0300

    added form validation

--- restful_shows/tv_shows/models.py
+++ restful_shows/tv_shows/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from datetime import datetime
+today = datetime.today()
+print(today)
 
-# Create your models here.
+
+class ShowsManager(models.Manager):
+    def show_validator(self, request_data):
+        errors = {}
+        if len(request_data['show_title']) < 5:
+            errors['title'] = 'Please enter a valid title'
+        if len(request_data['net_work']) < 5:
+            errors['network'] = 'Please enter a valid network'
+        if len(request_data['desc']) < 75:
+            errors['desc'] = 'description should be at least 70 charachters'
+        if request_data['release_date'] > str(today):
+            errors['release_date'] = 'please enter a valid date'
+        return errors
 
 
 class Shows(models.Model):
@@ -8,3 +23,4 @@ class Shows(models.Model):
     network = models.CharField(max_length=255)
     release_date = models.DateField()
     description = models.TextField(null=True)
+    objects = ShowsManager()
--- restful_shows/tv_shows/views.py
+++ restful_shows/tv_shows/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-
-import tv_shows
+from django.contrib import messages
 from . models import Shows
 
 
@@ -16,19 +15,26 @@ def new_show(request):
 
 
 def create_new_show(request):
-    title = request.POST.get('show_title')
-    net_work = request.POST.get('net_work')
-    release_date = request.POST.get('release_date')
-    description = request.POST.get('desc')
-    new_show = Shows.objects.create(
-        title=title,
-        network=net_work,
-        release_date=release_date,
-        description=description
-    )
-    new_show.save()
-    # new_show = Shows.objects.last()
-    return redirect(f'/new/{new_show.id}')
+    errors = Shows.objects.show_validator(request.POST)
+    print(errors)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/new')
+    else:
+        title = request.POST.get('show_title')
+        net_work = request.POST.get('net_work')
+        release_date = request.POST.get('release_date')
+        description = request.POST.get('desc')
+        new_show = Shows.objects.create(
+            title=title,
+            network=net_work,
+            release_date=release_date,
+            description=description
+        )
+        new_show.save()
+        # new_show = Shows.objects.last()
+        return redirect(f'/new/{new_show.id}')
 
 
 def view_show(request, id):
@@ -49,17 +55,23 @@ def view_show_to_edit(request, id):
 
 def edit_show(request, id):
     show = Shows.objects.get(id=id)
-    edited_title = request.POST.get('show_title')
-    print(edited_title)
-    edited_net_work = request.POST.get('net_work')
-    edited_release_date = request.POST.get('release_date')
-    edited_discription = request.POST.get('discription')
-    show.title = edited_title
-    show.network = edited_net_work
-    show.release_date = edited_release_date
-    show.disc = edited_discription
-    show.save()
-    return redirect(f'/{show.id}/edit')
+    errors = Shows.objects.show_validator(request.POST)
+    print(errors)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/{show.id}/edit')
+    else:
+        edited_title = request.POST.get('show_title')
+        edited_net_work = request.POST.get('net_work')
+        edited_release_date = request.POST.get('release_date')
+        edited_discription = request.POST['desc']
+        show.title = edited_title
+        show.network = edited_net_work
+        show.release_date = edited_release_date
+        show.description = edited_discription
+        show.save()
+        return redirect(f'/{show.id}/edit')
 
 
 def delete_show(request, id):
